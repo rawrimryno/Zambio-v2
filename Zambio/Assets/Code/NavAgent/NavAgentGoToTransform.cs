32fdeaf9f9120191682246ebd0ce13d642430dac
Todd <toddmoulder88@gmail.com>
2016-02-23 15:09:39 -0600

    Pulled the shells in.  Added a basic navmesh to the terrain.  NavAgentGoToTransform.cs allows any GameObject with a navagent component to continually go to and look at a Transform.  Slipped one on a red shell to have it destroy an enemy.  Slight bug, LookAt forces the object's transform to look at another.  The goomba is off in direction by 180 degrees and bloody koopa is either 90 or 270.  Small tweaks.

---
+++ Zambio/Assets/Code/NavAgent/NavAgentGoToTransform.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavAgentGoToTransform : MonoBehaviour
+{
+
+    public Transform target;
+    NavMeshAgent agent;
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+            agent.SetDestination(target.position);
+            transform.LookAt(target);
+        }
+    }
+}
