80cf0c320141f3a47baadd7464f2f8ff92f96806
Ryan Ngo <rawrimryno@gmail.com>
2016-02-23 18:19:54 -0600

    A lot of Changes....New player movement code and aura to shrooms
    Updates:
    -added player prefab to scene level_one
    -added aura to shrooms
    -esc to init mouse and pause
    -learned to use meshCollider with shells instead of capsule colliders

---
+++ Zambio/Assets/Code/Player/PlayerShoot.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShoot : MonoBehaviour {
+
+    public GameObject ammo;
+    public float bulletSpeed;
+    public float rateOfFire;
+    private float timer;
+    private Rigidbody projectile;
+
+	// Use this for initialization
+	void Start () {
+	    if (bulletSpeed == 0)
+        {
+            bulletSpeed = 20.0f;
+        }
+        if (rateOfFire == 0)
+        {
+            rateOfFire = 1.0f;
+        }
+        timer = rateOfFire;
+        projectile = ammo.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1") && rateOfFire <= 0)
+        {
+            Rigidbody clone;
+            clone = Instantiate(projectile, (transform.position), transform.rotation) as Rigidbody;
+            clone.velocity = transform.TransformDirection((Vector3.forward) * bulletSpeed);
+            Debug.Log("You Fired!");
+            rateOfFire = timer;
+        }
+        else
+        {
+            rateOfFire -= Time.deltaTime;
+        }
+	}
+}
