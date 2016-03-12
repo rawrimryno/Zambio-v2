07166d9f4c0b69fd137f9bf031c5251c2b1e3f81
Ryan Ngo <rawrimryno@gmail.com>
2016-03-11 21:32:59 -0600

    no message

--- Zambio/Assets/Code/Ammo/AmmoScript.cs
+++ Zambio/Assets/Code/Ammo/AmmoScript.cs
@@ -2,11 +2,17 @@
 using System.Collections;
 using System;
 
-public class AmmoScript : MonoBehaviour {
+public class AmmoScript : MonoBehaviour
+{
     private Rigidbody rb;
+    public int hitCounts;
+    private int hitsLeft;
+    private int ID;
+    public float speed;
 
     public float spinFactor;
 
+    NavMeshAgent meshAgent;
     // Called at the same time if it is in a sceneload, for all objects being loaded
     // Good to place calcs that are independent from other game objects here
     void Awake()
@@ -16,18 +22,26 @@ public class AmmoScript : MonoBehaviour {
         {
             spinFactor = 15f;
         }
+
+        if (this.gameObject.name == "redShell")
+        {
+            meshAgent = GetComponent<NavMeshAgent>();
+            meshAgent.speed = speed;
+        }
     }
 
-	// Use this for initialization
-	void Start () {
-        rb.AddTorque(rb.mass * spinFactor * Vector3.up); 
+    // Use this for initialization
+    void Start()
+    {
+        rb.AddTorque(rb.mass * spinFactor * Vector3.up);
     }
-	
-	// Update is called once per frame
-	void Update () {
 
-	}
-    
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
     // Interesting,
     // FixedUpdate() AddTorque doesn't seem to work with NavMesh and NavAgent
     // 
@@ -36,9 +50,9 @@ public class AmmoScript : MonoBehaviour {
 
     }
 
-    void OnCollisionEnter( Collision cInfo)
+    void OnCollisionEnter(Collision cInfo)
     {
-        if ( cInfo.transform.CompareTag("Enemy"))
+        if (cInfo.transform.CompareTag("Enemy"))
         {
             cInfo.gameObject.SetActive(false);
             Destroy(cInfo.gameObject);
